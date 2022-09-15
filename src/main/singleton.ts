1add4331f825cd7a580d655a062b85528564918d
Vincent Chan <okcdz@diverse.space>
2022-09-16 00:04:24 +0800

    feat: message to show window

--- src/main/singleton.ts
+++ src/main/singleton.ts
@@ -1,6 +1,7 @@
 import type { BrowserWindow } from "electron";
 
 interface Singleton {
+  welcomeWindow: BrowserWindow | undefined;
   browserWindow: BrowserWindow | undefined;
   appDataDir: string | undefined;
   userDataDir: string | undefined;
@@ -8,6 +9,7 @@ interface Singleton {
 }
 
 const singleton: Singleton = {
+  welcomeWindow: undefined,
   browserWindow: undefined,
   appDataDir: undefined,
   userDataDir: undefined,
